8caa5b059df7b758a50790c90ca5389c82be5a0b
CIHAN GUR <138358253+cihangr@users.noreply.github.com>
2023-11-18 19:11:25 +0200

    Numbers and rectangle area added

---
+++ rectangleArea/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int area = a*b;
+        System.out.println(area);
+    }
+}
