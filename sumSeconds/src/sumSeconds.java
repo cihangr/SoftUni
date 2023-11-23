74449f9804e80092280f22c413c62fbaf6c213a3
CIHAN GUR <138358253+cihangr@users.noreply.github.com>
2023-11-23 12:01:38 +0200

    Lessons Uptading

---
+++ sumSeconds/src/sumSeconds.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class sumSeconds {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int first = Integer.parseInt(inp.nextLine());
+        int second = Integer.parseInt(inp.nextLine());
+        int third = Integer.parseInt(inp.nextLine());
+        int totaltime = first+second+third;
+        int min = totaltime/60;
+        int sec = totaltime%60;
+        if (sec<10){
+            System.out.printf("%d:%02d" , min , sec);
+        } else {
+            System.out.printf("%d:%d" , min , sec);
+        }
+    }
+}
