3074bfd8d490148f8b335a54b6857a009c3846c7
CIHAN GUR <138358253+cihangr@users.noreply.github.com>
2023-11-30 15:13:22 +0200

    Conditional Statement Advanced 2 Completed

--- conditionalStatementAdvanced2/src/onTimeForExam.java
+++ conditionalStatementAdvanced2/src/onTimeForExam.java
@@ -7,10 +7,29 @@ public class onTimeForExam {
         int examMin = Integer.parseInt(inp.nextLine());
         int arrHour = Integer.parseInt(inp.nextLine());
         int arrMin = Integer.parseInt(inp.nextLine());
-        if (examMin>=30){
-            if (arrMin==(examMin-30))
-        } else {
+        int examTime = (examHour*60)+examMin;
+        int arrTime = (arrHour*60)+arrMin;
+        int diff = Math.abs(examTime-arrTime);
 
+        if (examTime<arrTime){
+            System.out.println("Late");
+            if (diff<60){
+                System.out.printf("%d minutes after the start",diff);
+            } if (diff>=60){
+                System.out.printf("%d:%02d hours after the start",(diff/60),(diff%60));
+            }
+        } else if (examTime==arrTime || diff<=30) {
+            System.out.println("On time");
+            if (diff!=0){
+                System.out.printf("%d minutes before the start",diff);
+            }
+        } else {
+            System.out.println("Early");
+            if (diff<60){
+                System.out.printf("%d minutes before the start",diff);
+            } if (diff>=60){
+                System.out.printf("%d:%02d hours before the start",(diff/60),(diff%60));
+            }
         }
     }
 }
---
+++ conditionalStatementAdvanced2/src/skiTrip.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class skiTrip {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int days = Integer.parseInt(inp.nextLine());
+        String type = inp.nextLine();
+        String assessment = inp.nextLine();
+        int room=0;
+        double discount=0, bill;
+
+        switch (type){
+            case "room for one person": room=18; break;
+            case "apartment": room=25;
+                if (days<=10){
+                    discount=0.3;
+                } else if (days<=15) {
+                    discount=0.35;
+                }else{
+                    discount=0.50;
+                }
+                break;
+            case "president apartment": room=35;
+                if (days<=10){
+                    discount=0.1;
+                } else if (days<=15) {
+                    discount=0.15;
+                }else{
+                    discount=0.20;
+                }
+                break;
+        }
+        bill = room * (days-1) * (1-discount);
+        if (assessment.equals("positive")){
+            bill=bill+(bill*0.25);
+        } else if (assessment.equals("negative")){
+            bill=bill-(bill*0.10);
+        }
+        System.out.printf("%.2f",bill);
+
+    }
+}
