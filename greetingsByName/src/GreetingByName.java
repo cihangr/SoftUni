4b0bd2ccae615e3dc9c7854b02dfa99d0bd23ab3
CIHAN GUR <138358253+cihangr@users.noreply.github.com>
2024-02-19 13:36:07 +0200

    Update

--- greetingsByName/src/GreetingByName.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class GreetingByName {
-    public static void main(String[] args) {
-        Scanner inp = new Scanner(System.in);
-        String name = inp.nextLine();
-        System.out.println("Hello, " + name + "!");
-    }
-}
